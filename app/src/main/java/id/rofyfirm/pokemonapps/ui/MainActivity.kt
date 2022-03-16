aad1d33172f57533ed6a5a25ca80580aa9366fcc
Rofy Firmansyah <rofyfirm@gmail.com>
2022-03-16 14:58:13 +0700

    Initial commit

---
+++ app/src/main/java/id/rofyfirm/pokemonapps/ui/MainActivity.kt
@@ -0,0 +1,13 @@
+package id.rofyfirm.pokemonapps.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import id.rofyfirm.pokemonapps.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
